1f34f4f4a14ff47c8510ba39e130c4b1faa69d2a
Appspia <appspia@gmail.com>
2022-06-14 22:58:54 +0900

    chore : Project Init

---
+++ TopApps/TopApps/TopAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopAppsApp.swift
+//  TopApps
+//
+//  Created by APPSPIA on 2022/06/14.
+//
+
+import SwiftUI
+
+@main
+struct TopAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
